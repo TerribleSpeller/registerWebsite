0eb6dad596f01083feb5101eaefcf10f6c8ca495
TerribleSpeller <54887256+TerribleSpeller@users.noreply.github.com>
2024-08-12 09:15:54 +0700

    All the new fixes

---
+++ js/markAsAttending.js
@@ -0,0 +1,21 @@
+
+import { auth, firebaseConfig} from './firebase-config.js'; //REMEMEBR TO DIRECT IT PROPERLY!
+import { onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.8.0/firebase-auth.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js';
+import { getDatabase, ref, set, remove } from 'https://www.gstatic.com/firebasejs/9.8.0/firebase-database.js';
+
+
+const app2 = initializeApp(firebaseConfig);
+const database2 = getDatabase(app2);
+
+async function markAsAttending(idTest) {
+  try {
+    const guestRef = ref(database2, 'guests/' + idTest);
+    await set(ref(guestRef, 'attending'), true);
+    console.log('Guest marked as attending successfully');
+  } catch (error) {
+    console.error('Error attending guest:', error);
+  }
+}
+
+export { markAsAttending }

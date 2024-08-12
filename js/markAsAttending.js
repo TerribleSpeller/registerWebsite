0e71055e9bc8f29100abed96cd48b5666a343f38
TerribleSpeller <54887256+TerribleSpeller@users.noreply.github.com>
2024-08-12 16:25:40 +0700

    Close the website

--- js/markAsAttending.js
+++ js/markAsAttending.js
@@ -2,7 +2,7 @@
 import { auth, firebaseConfig} from './firebase-config.js'; //REMEMEBR TO DIRECT IT PROPERLY!
 import { onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.8.0/firebase-auth.js';
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js';
-import { getDatabase, ref, set, remove } from 'https://www.gstatic.com/firebasejs/9.8.0/firebase-database.js';
+import { getDatabase, ref, set, remove, update } from 'https://www.gstatic.com/firebasejs/9.8.0/firebase-database.js';
 
 
 const app2 = initializeApp(firebaseConfig);

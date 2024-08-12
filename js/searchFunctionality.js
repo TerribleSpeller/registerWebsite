295a99579680ab5c2fe34ea4432b98c3e76f551e
TerribleSpeller <54887256+TerribleSpeller@users.noreply.github.com>
2024-08-12 10:57:28 +0700

    Search Patch

---
+++ js/searchFunctionality.js
@@ -0,0 +1,26 @@
+// searchFunctionality.js
+import { data, renderTable, rowsPerPage, currentPage } from './renderTable.js';
+
+let filteredData = {};
+
+document.getElementById('searchButton').addEventListener('click', () => {
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+    filteredData = Object.keys(data).filter(key => {
+        const row = data[key];
+        return Object.values(row).some(value => 
+            value.toString().toLowerCase().includes(searchInput)
+        );
+    }).reduce((obj, key) => {
+        obj[key] = data[key];
+        return obj;
+    }, {});
+
+    //currentPage = 1;
+    renderTable(currentPage, filteredData);
+});
+
+document.getElementById('searchButtonReset').addEventListener('click', () => {
+    filteredData = {};
+    //currentPage = 1;
+    renderTable(currentPage);
+});
